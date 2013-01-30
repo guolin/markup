33564c7b3d7d8c83d9dfc40cc75ef13730123b51
GuoLin <mr.guo.lin@gmail.com>
2013-01-31 00:40:38 +0800

    fix sort bug

--- markup.py
+++ markup.py
@@ -1,17 +1,34 @@
 # -*- coding: utf-8 -*- 
-from bottle import route, run, Bottle
-from bottle import jinja2_view as view
+"""
+transform markdown file to html
+make it with jinja2 template
+So this just is a blog toole.
+
+coder: guolin
+email: 54guolin#gmail.com 
+
+"""
+
 import os
 import shutil
-import markdown
 
+import markdown
 from jinja2 import Environment, FileSystemLoader
+
+# 配置文件
+ABSOLUTE_ROOT = u"/Users/guolin/Workspace/markup/src"
+DOC_ROOT = os.path.join(ABSOLUTE_ROOT, "root")
+WWW_ROOT = os.path.join(ABSOLUTE_ROOT, "www")
+
 
 class Doc():
 	"""
-	fullpath  /Users/guolin/root/01_创建新用户/08_asdf.md
+	Document Clase
+
+	For example:
+	fullpath  /Users/guolin/root/01_path/08_asdf.md
 	root /Users/guolin/root
-	path 01_创建新用户
+	path 01_path
 	name 08_asdf.md
 	ext = md
 
@@ -23,7 +40,11 @@ class Doc():
 	name = ""
 
 	def __init__(self, fullpath, root):
-		# fullpath = fullpath.decode("utf-8")
+		"""
+		init a Doc object with fullpath and root
+		eg Doc("/path/somethings/root/01_test","/path/somethings/root")
+		"""
+
 		self.root = root
 		path, filename = os.path.split(fullpath)
 		path = path[len(root):]
@@ -63,11 +84,17 @@ class Doc():
 	def is_same(self,obj):
 		return self.fullpath == obj.fullpath
 
+	def get_sort_id(self):
+		return self.id
+
 	def get_html(self):
 		return markdown.markdown(self.get_content().decode("utf-8"))
 
 
 class Folder():
+	"""
+	Folder Class
+	"""
 
 	def __init__(self, path, files, root):
 		fullpath = path
@@ -107,6 +134,9 @@ class Folder():
 		
 
 	def get_sub(self, folders):
+		"""
+		filter all the subfolder in a list(called folders)
+		"""
 		if self.sub_folders:
 			return self.sub_folders
 		for f in folders:
@@ -120,67 +150,48 @@ class Folder():
 				return d
 		return None
 
+	def get_sort_id(self):
+		return self.id
+
 	def get_docs(self):
 		return [x for x in self.docs if x.title != "index"]
 
 
 def save_to_db(db, path, files):
+	"""
+	init a folder and their docs
+	"""
 	name = os.path.split(path)[1]
 	if name in ["static","templates"]:
 		return None
-	c = Folder(path, files, doc_root)
+	c = Folder(path, files, DOC_ROOT)
 	db["folders"].append(c)
 	db["docs"] = db["docs"] + c.docs
 
+def init_db(db):
+	if not db.get("cached", False):
+		os.path.walk(DOC_ROOT, save_to_db, db)
 
-# 配置文件
-absolute_root = u"/Users/guolin/Workspace/help/src"
-doc_root = os.path.join(absolute_root, "root")
-www_root = os.path.join(absolute_root, "www")
+		# for order
+		db['folders'] = db['folders']
+		db['folders'].sort(key = Folder.get_sort_id)
+		db['docs'] = db['docs']
+		db['docs'].sort(key = Doc.get_sort_id)
+		db['cached'] = True
+	return db
 
-# 临时数据库
-db = {"folders":[], "docs":[], "cached":False}
-
-
-# hack Bottle
-def get_db(self):
-	if not self.db.get("cached", False):
-		os.path.walk(doc_root, save_to_db, self.db)
-		self.db['cached'] = True
-	return self.db
-Bottle.get_db = get_db
-
-app = Bottle()
-app.db = db
-
-@app.route('/')
-@view("post.html")
-def index():
-	db = app.get_db()
-	folders = db['folders']
-	docs = db['docs']
-	root_deep = folders[0].deep
-	file_root = db['root']
-
-	return {'db':db,
-			'folders':folders,
-			'docs':docs ,
-			'root_deep':root_deep,
-			'file_root':file_root}
-
-
-def sync_www():
+def sync_www(db):
 
 	# set the root 
-	db = app.get_db()
+
 	docs = db['docs']
 	folders = db['folders']
 	root_deep = folders[0].deep
 
 
 	# copy template
-	doc_templates_dir = os.path.join(doc_root, "templates")
-	templates_dir = os.path.join(absolute_root, "templates")
+	doc_templates_dir = os.path.join(DOC_ROOT, "templates")
+	templates_dir = os.path.join(ABSOLUTE_ROOT, "templates")
 	if os.path.exists(doc_templates_dir):
 		if os.path.exists(templates_dir):
 			shutil.rmtree(templates_dir)
@@ -189,7 +200,7 @@ def sync_www():
 	# copy static
 	for folder in folders:
 		if os.path.exists(os.path.join(folder.fullpath, "static")):
-			www_path = os.path.join(www_root ,folder.path.strip("/").strip("\\"), "static")
+			www_path = os.path.join(WWW_ROOT ,folder.path.strip("/").strip("\\"), "static")
 			if os.path.exists(www_path):
 				shutil.rmtree(www_path)
 			shutil.copytree(os.path.join(folder.fullpath, "static"), www_path)
@@ -201,20 +212,18 @@ def sync_www():
 
 	for doc in docs:
 		t = template.render({"folders":folders, "current_doc":doc})
-		fullpath = os.path.join(www_root, doc.url.strip("/").strip("\\"))
-		fullroot = os.path.join(www_root, doc.path.strip("/").strip("\\"))
+		fullpath = os.path.join(WWW_ROOT, doc.url.strip("/").strip("\\"))
+		fullroot = os.path.join(WWW_ROOT, doc.path.strip("/").strip("\\"))
 		if not os.path.exists(fullroot):
 			os.makedirs(fullroot)
 		f = open(fullpath,"w")
 		f.write(t.encode("utf-8"))
 		f.close()
 
-sync_www()
-
-
-
-
-# run(app, host='localhost', port=8080, debug=True)
+# init db
+db = {"folders":[], "docs":[], "cached":False}
+init_db(db)
+sync_www(db)
 
 
 
